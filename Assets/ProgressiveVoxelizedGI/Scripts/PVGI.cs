bd3ddc764a0905471fc1c1c902223a35b9bdec13
parikshit6321 <psaraswa@usc.edu>
2018-08-03 04:00:26 +0530

    Imported the PVGI package.

---
+++ Assets/ProgressiveVoxelizedGI/Scripts/PVGI.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PVGI : MonoBehaviour {
+
+	public enum DebugVoxelGrid {
+		PROGRESSIVE,
+		NORMAL,
+		IRRADIANCE
+	};
+
+	[Header("Debug Settings")]
+	public bool debugMode = false;
+	public DebugVoxelGrid debugVoxelGrid = DebugVoxelGrid.PROGRESSIVE;
+
+	[Header("Shaders")]
+	public Shader pvgiShader = null;
+	public ComputeShader voxelGridEntryShader = null;
+	public ComputeShader voxelGridCleanupShader = null;
+	public ComputeShader voxelGridRayTracerShader = null;
+
+	[Header("General Settings")]
+	public Vector2Int resolution = Vector2Int.zero;
+	public float indirectLightingStrength = 1.0f;
+
+	[Header("Voxelization Settings")]
+	public float worldVolumeBoundary = 10.0f;
+	public int voxelVolumeDimension = 32;
+	public float voxelizationDepth = 0.1f;
+
+	[Header("Ray Trace Settings")]
+	public float rayDistance = 30.0f;
+	public int maximumIterations = 50;
+
+	private RenderTexture lightingTexture = null;
+	private RenderTexture positionTexture = null;
+	private RenderTexture normalTexture = null;
+
+	private Material pvgiMaterial = null;
+
+	private RenderTextureDescriptor voxelGridDescriptorFloat4;
+
+	private struct VoxelGrid
+	{
+		// Direct Lit Color and occupied flag
+		public RenderTexture voxelGridProgressive;
+		public RenderTexture voxelGridIrradiance;
+		public RenderTexture voxelGridNormal;
+	};
+
+	private VoxelGrid voxelGrid;
+
+	// Use this for initialization
+	void Start () {
+
+		Screen.SetResolution (resolution.x, resolution.y, true);
+
+		GetComponent<Camera> ().depthTextureMode = DepthTextureMode.Depth | DepthTextureMode.DepthNormals;
+
+		if (pvgiShader != null) {
+
+			pvgiMaterial = new Material (pvgiShader);
+
+		}
+
+		InitializeVoxelGrid();
+
+		lightingTexture = new RenderTexture (voxelVolumeDimension, voxelVolumeDimension, 0, RenderTextureFormat.ARGBFloat);
+		positionTexture = new RenderTexture (voxelVolumeDimension, voxelVolumeDimension, 0, RenderTextureFormat.ARGBFloat);
+		normalTexture = new RenderTexture (voxelVolumeDimension, voxelVolumeDimension, 0, RenderTextureFormat.ARGBFloat);
+
+	}
+
+	// Function to initialize the voxel grid data
+	private void InitializeVoxelGrid() {
+
+		voxelGridDescriptorFloat4 = new RenderTextureDescriptor ();
+		voxelGridDescriptorFloat4.bindMS = false;
+		voxelGridDescriptorFloat4.colorFormat = RenderTextureFormat.ARGBFloat;
+		voxelGridDescriptorFloat4.depthBufferBits = 0;
+		voxelGridDescriptorFloat4.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+		voxelGridDescriptorFloat4.enableRandomWrite = true;
+		voxelGridDescriptorFloat4.height = voxelVolumeDimension;
+		voxelGridDescriptorFloat4.msaaSamples = 1;
+		voxelGridDescriptorFloat4.volumeDepth = voxelVolumeDimension;
+		voxelGridDescriptorFloat4.width = voxelVolumeDimension;
+		voxelGridDescriptorFloat4.sRGB = true;
+
+		voxelGrid.voxelGridProgressive = new RenderTexture (voxelGridDescriptorFloat4);
+		voxelGrid.voxelGridIrradiance = new RenderTexture (voxelGridDescriptorFloat4);
+		voxelGrid.voxelGridNormal = new RenderTexture (voxelGridDescriptorFloat4);
+
+		voxelGrid.voxelGridProgressive.filterMode = FilterMode.Bilinear;
+		voxelGrid.voxelGridIrradiance.filterMode = FilterMode.Bilinear;
+		voxelGrid.voxelGridNormal.filterMode = FilterMode.Bilinear;
+
+		voxelGrid.voxelGridProgressive.Create ();
+		voxelGrid.voxelGridIrradiance.Create ();
+		voxelGrid.voxelGridNormal.Create ();
+
+	}
+
+	// Function to update data in the voxel grid
+	private void UpdateVoxelGrid () {
+
+		// Kernel index for the entry point in compute shader
+		int kernelHandle = voxelGridEntryShader.FindKernel("CSMain");
+
+		// Updating voxel grid
+		voxelGridEntryShader.SetTexture(kernelHandle, "voxelGridProgressive", voxelGrid.voxelGridProgressive);
+		voxelGridEntryShader.SetTexture(kernelHandle, "voxelGridNormal", voxelGrid.voxelGridNormal);
+		voxelGridEntryShader.SetInt("voxelVolumeDimension", voxelVolumeDimension);
+		voxelGridEntryShader.SetFloat ("voxelizationDepth", voxelizationDepth);
+		voxelGridEntryShader.SetFloat ("worldVolumeBoundary", worldVolumeBoundary);
+		voxelGridEntryShader.SetTexture(kernelHandle, "lightingTexture", lightingTexture);
+		voxelGridEntryShader.SetTexture(kernelHandle, "positionTexture", positionTexture);
+		voxelGridEntryShader.SetTexture(kernelHandle, "normalTexture", normalTexture);
+
+		voxelGridEntryShader.Dispatch(kernelHandle, voxelVolumeDimension, voxelVolumeDimension, 1);
+
+	}
+
+	// Function to cleanup the voxel grid irradiance
+	private void CleanupVoxelGrid () {
+
+		// Kernel index for the entry point in compute shader
+		int kernelHandle = voxelGridCleanupShader.FindKernel("CSMain");
+		voxelGridCleanupShader.SetTexture(kernelHandle, "voxelGridIrradiance", voxelGrid.voxelGridIrradiance);
+		voxelGridCleanupShader.Dispatch(kernelHandle, voxelVolumeDimension, voxelVolumeDimension, voxelVolumeDimension);
+
+	}
+
+	// Function to ray trace through the voxel grid
+	private void RayTraceVoxelGrid () {
+	
+		// Kernel index for the entry point in compute shader
+		int kernelHandle = voxelGridCleanupShader.FindKernel("CSMain");
+		voxelGridRayTracerShader.SetTexture(kernelHandle, "voxelGridProgressive", voxelGrid.voxelGridProgressive);
+		voxelGridRayTracerShader.SetTexture(kernelHandle, "voxelGridNormal", voxelGrid.voxelGridNormal);
+		voxelGridRayTracerShader.SetTexture(kernelHandle, "voxelGridIrradiance", voxelGrid.voxelGridIrradiance);
+		voxelGridRayTracerShader.SetFloat("worldVolumeBoundary", worldVolumeBoundary);
+		voxelGridRayTracerShader.SetFloat("rayDistance", rayDistance);
+		voxelGridRayTracerShader.SetFloat("maximumIterations", (float)maximumIterations);
+		voxelGridRayTracerShader.SetInt("voxelVolumeDimension", voxelVolumeDimension);
+		voxelGridRayTracerShader.SetTexture(kernelHandle, "positionTexture", positionTexture);
+		voxelGridRayTracerShader.SetTexture(kernelHandle, "normalTexture", normalTexture);
+		voxelGridRayTracerShader.Dispatch(kernelHandle, voxelVolumeDimension, voxelVolumeDimension, voxelVolumeDimension);
+
+	}
+
+	// This is called once per frame after the scene is rendered
+	void OnRenderImage (RenderTexture source, RenderTexture destination) {
+
+		pvgiMaterial.SetTexture("voxelGridProgressive", voxelGrid.voxelGridProgressive);
+		pvgiMaterial.SetTexture("voxelGridIrradiance", voxelGrid.voxelGridIrradiance);
+		pvgiMaterial.SetTexture("voxelGridNormal", voxelGrid.voxelGridNormal);
+		pvgiMaterial.SetFloat ("worldVolumeBoundary", worldVolumeBoundary);
+		pvgiMaterial.SetMatrix ("InverseViewMatrix", GetComponent<Camera>().cameraToWorldMatrix);
+		pvgiMaterial.SetMatrix ("InverseProjectionMatrix", GetComponent<Camera>().projectionMatrix.inverse);
+		pvgiMaterial.SetInt ("voxelVolumeDimension", voxelVolumeDimension);
+		pvgiMaterial.SetFloat ("indirectLightingStrength", indirectLightingStrength);
+
+		Graphics.Blit (source, lightingTexture);
+		Graphics.Blit (source, positionTexture, pvgiMaterial, 0);
+		Graphics.Blit (source, normalTexture, pvgiMaterial, 1);
+
+		CleanupVoxelGrid ();
+
+		UpdateVoxelGrid ();
+
+		RayTraceVoxelGrid ();
+
+		if (debugMode) {
+
+			if (debugVoxelGrid == DebugVoxelGrid.PROGRESSIVE) {
+				Graphics.Blit (source, destination, pvgiMaterial, 2);
+			} else if (debugVoxelGrid == DebugVoxelGrid.NORMAL) {
+				Graphics.Blit (source, destination, pvgiMaterial, 3);
+			} else {
+				Graphics.Blit (source, destination, pvgiMaterial, 4);
+			}
+		} else {
+			Graphics.Blit (source, destination, pvgiMaterial, 5);
+		}
+
+	}
+
+}
---
+++ Assets/ProgressiveVoxelizedGI/Shaders/PVGIShader.shader
@@ -0,0 +1,265 @@
+﻿Shader "Hidden/PVGIShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		CGINCLUDE
+
+		#include "UnityCG.cginc"
+
+		#define PI 3.1415926f
+
+		uniform sampler3D				voxelGridProgressive;
+		uniform sampler3D				voxelGridIrradiance;
+		uniform sampler3D				voxelGridNormal;
+
+		uniform sampler2D 				_MainTex;
+		uniform sampler2D				_IndirectTex;
+		uniform sampler2D				_CameraDepthTexture;
+		uniform sampler2D				_CameraDepthNormalsTexture;
+		uniform sampler2D				_CameraGBufferTexture0;
+		uniform sampler2D				_CameraGBufferTexture1;
+
+		uniform float4x4				InverseProjectionMatrix;
+		uniform float4x4				InverseViewMatrix;
+
+		uniform float4					_MainTex_TexelSize;
+
+		uniform float					worldVolumeBoundary;
+		uniform float					indirectLightingStrength;
+
+		uniform int						voxelVolumeDimension;
+
+		struct appdata
+		{
+			float4 vertex : POSITION;
+			float2 uv : TEXCOORD0;
+		};
+
+		struct v2f
+		{
+			float2 uv : TEXCOORD0;
+			float4 vertex : SV_POSITION;
+			float4 cameraRay : TEXCOORD1;
+		};
+
+		v2f vert (appdata v)
+		{
+			v2f o;
+			o.vertex = UnityObjectToClipPos(v.vertex);
+			o.uv = v.uv;
+
+			//transform clip pos to view space
+			float4 clipPos = float4( v.uv * 2.0f - 1.0f, 1.0f, 1.0f);
+			float4 cameraRay = mul(InverseProjectionMatrix, clipPos);
+			o.cameraRay = cameraRay / cameraRay.w;
+
+			return o;
+		}
+
+		float3 EncodePosition (float3 inputPosition)
+		{
+			float3 encodedPosition = inputPosition / worldVolumeBoundary;
+			encodedPosition += float3(1.0f, 1.0f, 1.0f);
+			encodedPosition /= 2.0f;
+			return encodedPosition;
+		}
+
+		float4 frag_position (v2f i) : SV_Target
+		{
+			// read low res depth and reconstruct world position
+			float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+			
+			//linearise depth		
+			float lindepth = Linear01Depth (depth);
+			
+			//get view and then world positions		
+			float4 viewPos = float4(i.cameraRay.xyz * lindepth, 1.0f);
+			float3 worldPos = mul(InverseViewMatrix, viewPos).xyz;
+
+			return float4(worldPos, lindepth);
+		}
+
+		float4 frag_normal (v2f i) : SV_Target
+		{
+			float depthValue;
+			float3 viewSpaceNormal;
+			DecodeDepthNormal(tex2D(_CameraDepthNormalsTexture, i.uv), depthValue, viewSpaceNormal);
+			viewSpaceNormal = normalize(viewSpaceNormal);
+			float3 worldSpaceNormal = mul((float3x3)InverseViewMatrix, viewSpaceNormal);
+			worldSpaceNormal = normalize(worldSpaceNormal);
+			return float4(worldSpaceNormal, 1.0f);
+		}
+
+		// Returns the voxel information from grid
+		inline float4 GetVoxelInfo(float3 voxelPosition)
+		{
+			float4 info = tex3D(voxelGridProgressive, voxelPosition);
+			return info;
+		}
+
+		// Returns the world-space normal stored in the voxel
+		inline float3 GetVoxelNormal(float3 voxelPosition)
+		{
+			float3 worldNormal = tex3D(voxelGridNormal, voxelPosition).rgb;
+			return worldNormal;
+		}
+
+		// Returns the irradiance stored in the voxel
+		inline float3 GetVoxelIrradiance(float3 voxelPosition)
+		{
+			float3 irradiance = tex3D(voxelGridIrradiance, voxelPosition).rgb;
+			return irradiance;
+		}
+
+		// Function to get position of voxel in the grid
+		inline float3 GetVoxelPosition (float3 worldPosition)
+		{
+			float3 encodedPosition = worldPosition / worldVolumeBoundary;
+			encodedPosition += float3(1.0f, 1.0f, 1.0f);
+			encodedPosition /= 2.0f;
+			return encodedPosition;
+		}
+
+		float4 frag_debug_progressive (v2f i) : SV_Target
+		{
+			// read low res depth and reconstruct world position
+			float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+			
+			//linearise depth		
+			float lindepth = Linear01Depth (depth);
+			
+			//get view and then world positions		
+			float4 viewPos = float4(i.cameraRay.xyz * lindepth, 1.0f);
+			float3 worldPos = mul(InverseViewMatrix, viewPos).xyz;
+
+			float4 voxelInfo = GetVoxelInfo(GetVoxelPosition(worldPos));
+			return voxelInfo;
+
+		}
+
+		float4 frag_debug_normal (v2f i) : SV_Target
+		{
+			// read low res depth and reconstruct world position
+			float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+			
+			//linearise depth		
+			float lindepth = Linear01Depth (depth);
+			
+			//get view and then world positions		
+			float4 viewPos = float4(i.cameraRay.xyz * lindepth, 1.0f);
+			float3 worldPos = mul(InverseViewMatrix, viewPos).xyz;
+
+			float3 voxelNormal = GetVoxelNormal(GetVoxelPosition(worldPos));
+			return float4(voxelNormal, 1.0f);
+
+		}
+
+		float4 frag_debug_irradiance (v2f i) : SV_Target
+		{
+			// read low res depth and reconstruct world position
+			float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+			
+			//linearise depth		
+			float lindepth = Linear01Depth (depth);
+			
+			//get view and then world positions		
+			float4 viewPos = float4(i.cameraRay.xyz * lindepth, 1.0f);
+			float3 worldPos = mul(InverseViewMatrix, viewPos).xyz;
+
+			float3 irradiance = GetVoxelIrradiance(GetVoxelPosition(worldPos));
+			return float4(irradiance, 1.0f);
+
+		}
+
+		float4 frag_lighting (v2f i) : SV_Target
+		{
+			float metallic = tex2D (_CameraGBufferTexture1, i.uv).r;
+
+			// read low res depth and reconstruct world position
+			float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+			
+			//linearise depth		
+			float lindepth = Linear01Depth (depth);
+			
+			//get view and then world positions		
+			float4 viewPos = float4(i.cameraRay.xyz * lindepth, 1.0f);
+			float3 worldPos = mul(InverseViewMatrix, viewPos).xyz;
+
+			float4 gBufferSample = tex2D(_CameraGBufferTexture0, i.uv);
+			float3 albedo = gBufferSample.rgb;
+			float ao = gBufferSample.a;
+
+			float3 voxelIrradiance = GetVoxelIrradiance(GetVoxelPosition(worldPos));
+
+			float3 direct = tex2D(_MainTex, i.uv).rgb;
+			float3 indirect = (((ao * indirectLightingStrength * (1.0f - metallic)) / PI) * (albedo * voxelIrradiance));
+			float3 finalLighting = direct + indirect;
+
+			return float4(finalLighting, 1.0f);
+
+		}
+
+		ENDCG
+
+		// 0 : World Position Writing pass
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag_position
+			ENDCG
+		}
+
+		// 1 : World Normal Writing pass
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag_normal
+			ENDCG
+		}
+
+		// 2 : Progressive Debug pass
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag_debug_progressive
+			ENDCG
+		}
+
+		// 3 : Normal Debug pass
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag_debug_normal
+			ENDCG
+		}
+
+		// 4: Irradiance Debug pass
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag_debug_irradiance
+			ENDCG
+		}
+
+		// 3 : Lighting accumulation pass
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag_lighting
+			ENDCG
+		}
+	}
+}
